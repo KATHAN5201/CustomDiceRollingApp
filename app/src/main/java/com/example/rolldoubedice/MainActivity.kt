551fc403cfcae2495243fecd7f868c946ce3f66f
Kathan <kathankv5201@gmail.com>
2020-06-05 16:35:12 +0530

    Added images views of dices

--- app/src/main/java/com/example/rolldoubedice/MainActivity.kt
+++ app/src/main/java/com/example/rolldoubedice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rolldoubedice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -15,18 +16,37 @@ class MainActivity : AppCompatActivity() {
 
         //getting required references
         val button: Button = findViewById(R.id.roll_dice)
-        val diceTextViews: Array<TextView> = arrayOf(
-            findViewById(R.id.Dice1),
-            findViewById(R.id.Dice2)
+        val diceImgViews: Array<ImageView> = arrayOf(
+            findViewById(R.id.Vdice_1),
+            findViewById(R.id.Vdice_2)
         )
 
-        button.setOnClickListener { rollDices(dices, diceTextViews) }
+        //rolling dices on clicking roll button
+        button.setOnClickListener { rollDices(dices, diceImgViews) }
+
+        //rolling both dice once
+        rollDices(dices,diceImgViews)
 
     }
 
-    private fun rollDices(dices: Array<Dice>, diceTextViews: Array<TextView>) {
-        for (i in 0..1) {
-            diceTextViews[i].text = dices[i].roll().toString()
+    private fun rollDices(dices: Array<Dice>, diceImgViews: Array<ImageView>) {
+
+        for (diceNumber in 0..1) {
+            //rolling dice at every iteration
+            val diceRoll = dices[diceNumber].roll();
+            val drawableResource = when (diceRoll) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            //updating the image resource depending on resulting dice
+            diceImgViews[diceNumber].setImageResource(drawableResource)
+            //updating content description for accessibility purpose
+            diceImgViews[diceNumber].contentDescription = diceRoll.toString()
         }
     }
 }
